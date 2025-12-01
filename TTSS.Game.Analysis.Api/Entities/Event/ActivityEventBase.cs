f0325cccf44e64fe8cd118662dc8cb3ad46dbcf4
villsource <30929839+VillSource@users.noreply.github.com>
2025-12-01 12:26:45 +0700

    feat(entity): add event model

---
+++ TTSS.Game.Analysis.Api/Entities/Event/ActivityEventBase.cs
@@ -0,0 +1,9 @@
+﻿namespace TTSS.Game.Analysis.Api.Entities.Event;
+
+public class ActivityEventBase
+{
+    public Guid? EventId { get; set; }
+    public string EventName { get; set; } = string.Empty;
+    public string UserId { get; set; } = string.Empty;
+    public DateTime Timestamp { get; set; }
+}
---
+++ TTSS.Game.Analysis.Api/Entities/Event/PurchasedEvent.cs
@@ -0,0 +1,9 @@
+﻿namespace TTSS.Game.Analysis.Api.Entities.Event;
+
+public class PurchasedEvent: ActivityEventBase
+{
+    public string ItemId { get; set; } = string.Empty;
+    public string ItemName { get; set; } = string.Empty;
+    public int Quantity { get; set; }
+    public decimal? PricePerUnit { get; set; }
+}
