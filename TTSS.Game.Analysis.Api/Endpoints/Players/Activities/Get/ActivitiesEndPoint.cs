83b83ac18f39984e072ffc7b9968aae8fdfebe07
villsource <30929839+VillSource@users.noreply.github.com>
2025-12-01 14:22:50 +0700

    feat(activity): summary all log to get KPI per user/player

---
+++ TTSS.Game.Analysis.Api/Endpoints/Players/Activities/Get/ActivitiesEndPoint.cs
@@ -0,0 +1,48 @@
+﻿using FastEndpoints;
+using Microsoft.EntityFrameworkCore;
+using TTSS.Game.Analysis.Api.Constants;
+using TTSS.Game.Analysis.Api.Data;
+using TTSS.Game.Analysis.Api.Entities.Event;
+using TTSS.Game.Analysis.Api.Services;
+
+namespace TTSS.Game.Analysis.Api.Endpoints.Players.Activities.Get;
+
+public class ActivitiesEndPoint : Ep.Req<ActivitiesRequest>.Res<List<ActivitiesResponse>>
+{
+    private readonly ApplicationDbContext _db;
+    private readonly ServerTimeProvider _timeProvider;
+
+    public ActivitiesEndPoint(ApplicationDbContext db, ServerTimeProvider timeProvider)
+    {
+        _db = db;
+        _timeProvider = timeProvider;
+    }
+
+    public override void Configure()
+    {
+        Get("players/activities/{FromNDayAgo}");
+        Policies(AuthPolicyConstant.Admin);
+    }
+
+    public override async Task HandleAsync(ActivitiesRequest request, CancellationToken ct)
+    {
+        var now = _timeProvider.UtcNow();
+        var startPoint = now - TimeSpan.FromDays(request.FromNDayAgo);
+
+        var x = _db.Activities.AsNoTracking()
+            .Where(@event => @event.Timestamp >= startPoint)
+            .Where(@event => @event.Timestamp <= now)
+            .OrderBy(@event => @event.Timestamp)
+            .GroupBy(@event => @event.UserId)
+            .Select(@event => new ActivitiesResponse
+            {
+                UserId = @event.Key,
+                TotalLogin = @event.OfType<LoginEvent>().Count(),
+                TotalOnlineTime = TimeSpan.FromSeconds(@event.OfType<LogoutEvent>().Sum(logout => logout.DurationInSeconds)),
+                AchievementCount = @event.OfType<AchievementCompletedEvent>().Count(),
+                PurchasedCount = @event.OfType<PurchasedEvent>().Count(),
+                MoneySpend = Convert.ToDecimal( @event.OfType<PurchasedEvent>().Select(p => p.Quantity * p.PricePerUnit).Sum())
+            });
+        Response = await x.ToListAsync(ct);
+    }
+}
