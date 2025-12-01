30a716d156e232059c8ca9d143e1f798bba09797
villsource <30929839+VillSource@users.noreply.github.com>
2025-12-01 15:39:16 +0700

    feat(churning): add endpoint

---
+++ TTSS.Game.Analysis.Api/Endpoints/Players/Churnings/ChurningsEndpoint.cs
@@ -0,0 +1,36 @@
+﻿using FastEndpoints;
+using Microsoft.EntityFrameworkCore;
+using TTSS.Game.Analysis.Api.Constants;
+using TTSS.Game.Analysis.Api.Data;
+
+namespace TTSS.Game.Analysis.Api.Endpoints.Players.Churnings;
+
+public class ChurningsEndpoint : Ep.NoReq.Res<List<ChurningsResponse>>
+{
+    private readonly ApplicationDbContext _db;
+
+    public ChurningsEndpoint(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+
+    public override void Configure()
+    {
+        Get("players/churnings");
+        Policies(AuthPolicyConstant.Admin);
+    }
+
+    public override async Task HandleAsync(CancellationToken ct)
+    {
+        var x = _db.Churnings.AsNoTracking()
+            .Where(churning => churning.IsValid)
+            .GroupBy(churning => churning.UserId)
+            .Select(churning => new ChurningsResponse
+            {
+                DetectedAt = churning.First().DetecedTime,
+                Reason = churning.Select(c => c.ChurningReason).ToList(),
+                UserId = churning.Key
+            });
+        Response = await x.ToListAsync(ct);
+    }
+}
---
+++ TTSS.Game.Analysis.Api/Endpoints/Players/Churnings/ChurningsResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace TTSS.Game.Analysis.Api.Endpoints.Players.Churnings;
+
+public class ChurningsResponse
+{
+    public string UserId { get; set; } = string.Empty;
+    public List<string> Reason { get; set; } = [];
+    public DateTime DetectedAt { get; set; }
+}
