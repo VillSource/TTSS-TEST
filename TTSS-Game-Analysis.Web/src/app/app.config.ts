e7f6f7a3159f64680e881172435a1a5677b4f302
villsource <30929839+VillSource@users.noreply.github.com>
2025-12-01 19:53:56 +0700

    feat(ui): fast add simple ui (`angular`)

--- TTSS-Game-Analysis.Web/src/app/app.config.ts
+++ TTSS-Game-Analysis.Web/src/app/app.config.ts
@@ -1,5 +1,10 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
 
 import { routes } from './app.routes';
 
@@ -7,6 +12,13 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    providePrimeNG({
+        theme: {
+            preset: Aura
+        }
+    }),
+    provideHttpClient(),
   ]
 };
